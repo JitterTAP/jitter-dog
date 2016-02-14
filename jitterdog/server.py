9dc75a358dd331f269fb0cd338fcce93582e9f3a
kevin1chun <kevin@urx.com>
2016-02-14 13:31:05 -0800

    Write out

--- jitterdog/server.py
+++ jitterdog/server.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import logging
 import tornado.web
+import tornado.httpclient
 import tornado.ioloop
 import tornado.gen
 import tornado.websocket
@@ -16,10 +17,22 @@ change_events = tornado.locks.Event()
 clients = []
 
 class JenkinsHandler(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
     def get(self):
+        print("HITTTTTIN")
+        location = self.get_argument('location',None)
+        branch = self.get_argument("branch", None)
+        print(branch, location)
+        link = 'https://raw.githubusercontent.com/JitterTAP/jitter-bug/' + branch +  '/assets/css/main.css?thing=' + str(uuid.uuid4())
+        print(link)
+        client = tornado.httpclient.AsyncHTTPClient()
+        response = yield client.fetch(link)
+        print(response.body)
+        with open('/mnt/jittertap_' + location + '/jitter-bug/assets/css/main.css', 'wb') as f:
+            f.write(response.body)
         for client in clients:
             msg = json.dumps({'changed': True})
-            client.write_message()
+            client.write_message(msg)
 
 class HealthCheckHandler(tornado.web.RequestHandler): # noqa
     def set_default_headers(self):
@@ -56,26 +69,26 @@ class JitterDogHandler(tornado.websocket.WebSocketHandler): # noqa
 
 
 
-ROUTES = [
-    (r'/health', HealthCheckHandler),
-    (r'/jitterdog', JitterDogHandler),
-    (r'/jenkins', JenkinsHandler)
-]
-
-
-APPLICATION = tornado.web.Application(ROUTES)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Who let the dogs out?')
     parser.add_argument('--path', type=str)
+    parser.add_argument('--region', type=int)
     args = parser.parse_args()
-
-    APPLICATION.listen(settings.PORT)
-    logging.info('Serving on port: %d', settings.PORT)
+    ROUTES = [
+    (r'/health', HealthCheckHandler),
+    (r'/jitterdog', JitterDogHandler),
+    (r'/jenkins', JenkinsHandler),
+    (r'/(.+)', tornado.web.StaticFileHandler, {'path': args.path})
+    ]
+    APPLICATION = tornado.web.Application(ROUTES)
+    if args.region is 1:
+        APPLICATION.listen(15000)
+    else:
+        APPLICATION.listen(16000)
     jitter_dog.set_path(args.path)
-    ROUTES.append((r'/(.+)', tornado.web.StaticFileHandler, {'path': jitter_dog.path}))
-    jitter_dog.start()
+    print("starting server") 
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
